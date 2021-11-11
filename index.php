f665e661218e7d2d33553fef569b4366edd1dc3d
shimpei ohtani <ohtani.shimpei@gmail.com>
2021-11-11 14:36:15 +0900

    header nav add

--- index.php
+++ index.php
@@ -48,12 +48,42 @@ $todos = getTodos($pdo);
   <meta charset="utf-8">
   <title>PHP Todo App</title>
   <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
   <div id="app">
     <header class="header">
       <h1>Todo App</h1>
+      <nav class="pc-menu">
+        <ul class="dropdown">
+          <li><a href="https://github.com/shim369" target="_blank">Github</a></li>
+          <li><a href="https://dotinstall.com/home" target="_blank">Dotinstall</a></li>
+          <li class="menu__single">
+            <a href="">Learn</a>
+            <ul class="menu__second-level">
+              <li><a href="https://leetcode.com/" target="_blank">LeetCode</a></li>
+              <li><a href="https://jsprimer.net/" target="_blank">JavaScript Primer</a></li>
+              <li><a href="https://prog-8.com/dashboard" target="_blank">Progate</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <div class="sp-menu">
+        <span id="open-menu" class="material-icons">menu</span>
+      </div> 
     </header>
+    <div class="overlay">
+      <span id="close-menu" class="material-icons">close</span>
+      <nav>
+        <ul>
+          <li><a href="https://github.com/shim369" target="_blank">Github</a></li>
+          <li><a href="https://dotinstall.com/home" target="_blank">Dotinstall</a></li>
+          <li><a href="https://leetcode.com/" target="_blank">LeetCode</a></li>
+          <li><a href="https://jsprimer.net/" target="_blank">JavaScript Primer</a></li>
+          <li><a href="https://prog-8.com/dashboard" target="_blank">Progate</a></li>
+        </ul>
+      </nav>
+    </div>
     <div class="wrap">
     <main class="main">
       <section class="add-list">
--- js/main.js
+++ js/main.js
@@ -18,4 +18,18 @@
       span.parentNode.submit();
     });
   });
+  //hamburgerMenu
+  const openMenu = document.getElementById('open-menu');
+  const overlay = document.querySelector('.overlay');
+  const closeMenu = document.getElementById('close-menu');
+
+  openMenu.addEventListener('click',()=>{
+    overlay.classList.add('show-menu');
+    openMenu.classList.add('hide');
+  });
+  closeMenu.addEventListener('click',()=>{
+    overlay.classList.remove('show-menu');
+    openMenu.classList.remove('hide');
+  });
+  
 }
