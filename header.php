c9d07bea1e04ccde00eaaa2548b8d97107b8e2c2
shimpei ohtani <ohtani.shimpei@gmail.com>
2021-11-21 15:34:10 +0900

    nav common

--- header.php
+++ header.php
@@ -1,32 +1,11 @@
 <header class="header">
       <h1><a href="./">Todo App</a></h1>
-      <nav class="pc-menu">
-        <ul class="dropdown">
-          <li><a href="https://github.com/shim369" target="_blank">Github</a></li>
-          <li><a href="https://dotinstall.com/home" target="_blank">Dotinstall</a></li>
-          <li class="menu__single">
-            <a href="">More</a>
-            <ul class="menu__second-level">
-              <li><a href="https://leetcode.com/" target="_blank">LeetCode</a></li>
-              <li><a href="https://jsprimer.net/" target="_blank">JavaScript Primer</a></li>
-              <li><a href="https://prog-8.com/dashboard" target="_blank">Progate</a></li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
+      <?php include('nav.php') ?>
       <div class="sp-menu">
         <span id="open-menu" class="material-icons">menu</span>
       </div> 
     </header>
     <div class="overlay">
       <span id="close-menu" class="material-icons">close</span>
-      <nav>
-        <ul>
-          <li><a href="https://github.com/shim369" target="_blank">Github</a></li>
-          <li><a href="https://dotinstall.com/home" target="_blank">Dotinstall</a></li>
-          <li><a href="https://leetcode.com/" target="_blank">LeetCode</a></li>
-          <li><a href="https://jsprimer.net/" target="_blank">JavaScript Primer</a></li>
-          <li><a href="https://prog-8.com/dashboard" target="_blank">Progate</a></li>
-        </ul>
-      </nav>
+      <?php include('nav.php') ?>
     </div>
